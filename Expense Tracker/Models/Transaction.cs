838a7649d32002c8aa4abfc445005252a0bc53ae
dihaxn <ihanlaknuka@gmail.com>
2025-07-23 09:11:27 +0530

    a

--- Expense Tracker/Models/Transaction.cs
+++ Expense Tracker/Models/Transaction.cs
@@ -4,4 +4,11 @@ public class Transaction
 {
 	[key]
 	public int TransactionId { get; set; }
+
+	//CategoryId
+
+	public int Amount { get; set; }
+	public string? Note { get; set; }
+	public int MyProperty { get; set; }
+
 }
